1f3941dbcf5c57dd6abeee58cb3b7401e77762c5
christian <christiancelsomamanisoliz0@gmail.com>
2022-04-19 17:31:23 -0400

    verificación2

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -21,7 +21,7 @@ class EmailVerificationNotificationController extends Controller
         }
 
         $request->user()->sendEmailVerificationNotification();
-//return "verifique su correo";
+
         return back()->with('status', 'verification-link-sent');
     }
 }
